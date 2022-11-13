918116a1b15073a20eb85d8daa43e1fbd702f050
chrisliu298 <yuhaoliu298@gmail.com>
2022-11-13 13:58:46 -0800

    Save csv and plot

--- src/mi_estimation/model.py
+++ src/mi_estimation/model.py
@@ -1,7 +1,12 @@
+import datetime
+
+import matplotlib.pyplot as plt
+import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from matplotlib import colors
 from pytorch_lightning import LightningModule
 from torchinfo import summary
 from torchmetrics.functional import accuracy
@@ -13,6 +18,8 @@ class BaseModel(LightningModule):
     def __init__(self, config):
         super().__init__()
         self.config = config
+        self.layer_i_xt = []
+        self.layer_i_yt = []
 
     def on_train_start(self):
         input_size = self.trainer.datamodule.x_train.shape[1]
@@ -47,6 +54,25 @@ class BaseModel(LightningModule):
         acc = torch.stack([i["train_acc"] for i in outputs]).double().mean()
         self.log_dict({"avg_train_acc": acc, "avg_train_loss": loss}, logger=True)
         self.log_dict(weight_norm(self), logger=True)
+        if self.config.log_mi:
+            # estimate mutual information
+            x_train = self.trainer.datamodule.x_train
+            y_train = self.trainer.datamodule.y_train
+            x_train_id = self.trainer.datamodule.x_train_id
+            with torch.no_grad():
+                _, Ts = self(x_train.to(self.device))
+            layer_i_xt_at_epoch, layer_i_yt_at_epoch = {}, {}
+            for layer_idx, t in enumerate(Ts, 1):
+                t = t.cpu()
+                i_xt, i_yt = calculate_layer_mi(
+                    t, self.config.num_bins, self.config.activation, x_train_id, y_train
+                )
+                # self.log(f"l{layer_idx}_i_xt", i_xt, logger=True)
+                # self.log(f"l{layer_idx}_i_yt", i_yt, logger=True)
+                layer_i_xt_at_epoch[f"l{layer_idx}_i_xt"] = i_xt
+                layer_i_yt_at_epoch[f"l{layer_idx}_i_yt"] = i_yt
+            self.layer_i_xt.append(layer_i_xt_at_epoch)
+            self.layer_i_yt.append(layer_i_yt_at_epoch)
 
     def validation_step(self, batch, batch_idx):
         loss, acc = self.evaluate(batch, stage="val")
@@ -56,20 +82,6 @@ class BaseModel(LightningModule):
         loss = torch.stack([i["val_loss"] for i in outputs]).double().mean()
         acc = torch.stack([i["val_acc"] for i in outputs]).double().mean()
         self.log_dict({"avg_val_acc": acc, "avg_val_loss": loss}, logger=True)
-        if self.config.log_mi:
-            # estimate mutual information
-            x_train = self.trainer.datamodule.x_train
-            y_train = self.trainer.datamodule.y_train
-            x_train_id = self.trainer.datamodule.x_train_id
-            with torch.no_grad():
-                _, Ts = self(x_train.to(self.device))
-            for layer_idx, t in enumerate(Ts, 1):
-                t = t.cpu()
-                i_xt, i_yt = calculate_layer_mi(
-                    t, self.config.num_bins, self.config.activation, x_train_id, y_train
-                )
-                self.log(f"l{layer_idx}_i_xt", i_xt, logger=True)
-                self.log(f"l{layer_idx}_i_yt", i_yt, logger=True)
 
     def test_step(self, batch, batch_idx):
         loss, acc = self.evaluate(batch, "test")
@@ -79,6 +91,30 @@ class BaseModel(LightningModule):
         acc = torch.stack([i["test_acc"] for i in outputs]).double().mean()
         loss = torch.stack([i["test_loss"] for i in outputs]).double().mean()
         self.log_dict({"avg_test_acc": acc, "avg_test_loss": loss}, logger=True)
+
+    def on_train_end(self):
+        # save mutual information to csv and plot
+        current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        df_i_xt = pd.DataFrame(self.layer_i_xt)
+        df_i_xt.to_csv(f"i_xt_{current_time}.csv", index=False)
+        df_i_yt = pd.DataFrame(self.layer_i_yt)
+        df_i_yt.to_csv(f"i_yt_{current_time}.csv", index=False)
+        fig, ax = plt.subplots(figsize=(8, 6))
+        ax.set_xlabel(r"$I(X; T)$")
+        ax.set_ylabel(r"$I(Y; T)$")
+        num_layers = self.config.layer_shapes.count("x")
+        for i in range(num_layers):
+            a = ax.scatter(
+                df_i_xt[f"l{i+1}_i_xt"],
+                df_i_yt[f"l{i+1}_i_yt"],
+                s=50,
+                c=df_i_xt["epoch"],
+                cmap="plasma",
+                norm=colors.LogNorm(vmin=1, vmax=df_i_xt["epoch"].max()),
+            )
+        fig.colorbar(a, label="Epoch")
+        fig.savefig(f"mi_{current_time}.pdf", bbox_inches="tight")
+        fig.savefig(f"mi_{current_time}.png", bbox_inches="tight", dpi=600)
 
     def configure_optimizers(self):
         if self.config.optimizer == "adam":
