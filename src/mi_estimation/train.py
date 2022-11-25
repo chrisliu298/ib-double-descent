56f6c8c1dee2ddf142cc6e696462f11eeb13bb9b
Brendan King <bking2@ucsc.edu>
2022-11-25 15:36:10 -0800

    Adding method to run from cfg file (JSON), adding toy-synthetic template, run-files

--- src/mi_estimation/train.py
+++ src/mi_estimation/train.py
@@ -1,5 +1,8 @@
 import argparse
 import os
+import sys
+import json
+from typing import Optional
 
 import wandb
 from easydict import EasyDict
@@ -18,6 +21,17 @@ from model import CNN, FCN, RFN
 
 
 def parse_args():
+    # An alternative way to define and use arguments: define all as JSON and just load
+    # from file. Only used if first CLI argument is an existing file (presumed JSON format)
+    # `python train.py folder/my_run_spec.json`
+    #
+    # (e.g. if running a bunch of variants from a folder)
+    if os.path.exists(sys.argv[1]):
+        run_file: str = sys.argv[1]
+        with open(run_file, 'r') as f:
+            cfg = EasyDict(json.load(f))
+            return cfg
+
     # Parse command line arguments
     parser = argparse.ArgumentParser()
     # Dataset
@@ -64,6 +78,7 @@ def parse_args():
         "--optimizer", type=str, default="adam", choices=["adam", "sgd"]
     )
     parser.add_argument("--num_workers", default="full")
+    parser.add_argument("--work_dir", default=None)
     # MI estimation
     parser.add_argument("--num_bins", type=int, default=30)
     # Experiment
@@ -114,11 +129,16 @@ def main():
             mode="max",
         ),
     ]
+    # default is None, uses current working directory if set to None on logger
+    work_dir: Optional[str] = cfg.get('work_dir', None)
+    if work_dir and not os.path.exists(work_dir):
+        os.makedirs(work_dir, exist_ok=False)
     logger = WandbLogger(
         offline=not cfg.wandb,
         project=cfg.project_id,
         entity="ib-double-descent",
         config=cfg,
+        dir=work_dir
     )
     trainer = Trainer(
         accelerator="gpu",
